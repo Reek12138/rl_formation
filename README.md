3f2d2acbe70e3b8243a0932be39075c649d805b8
reek <2894487027@qq.com>
2024-05-16 18:39:21 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-“# rl_formation”
+learning-based multi-agent foramtion    
+
+# 领航跟随  
+领航者具有动力学模型的小车   
+跟随者为无人机  
--- env/rvo_inter.py
+++ env/rvo_inter.py
@@ -1,4 +1,4 @@
-from gym_env.envs.reciprocal_vel_obs import reciprocal_vel_obs
+from reciprocal_vel_obs import reciprocal_vel_obs
 from math import sqrt, atan2, asin, sin, pi, cos, inf
 import numpy as np
 
