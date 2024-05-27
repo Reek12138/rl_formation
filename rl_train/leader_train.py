d77c3de96af0d0cc6294a0fd8b67f9521d25bad6
Reek12138 <2894487027@qq.com>
2024-05-27 12:53:34 +0800

    update

--- rl_train/leader_train.py
+++ rl_train/leader_train.py
@@ -1,6 +1,3 @@
-import gym
-# import test_env
-# from pettingzoo.mpe import simple_adversary_v3
 import numpy as np
 import torch
 import torch.nn as nn
@@ -35,11 +32,7 @@ HIDDEN_DIM = 64
 GAMMA = 0.99
 TAU = 0.01
 
-#1. initialize the agent
-# env = simple_adversary_v3.parallel_env(N= 2, max_cycles = NUM_STEP, continuous_actions = True)
 env = CustomEnv(delta=0.1)
-# print("reset1==========================")
-
 multi_obs, infos = env.reset()
 #这里要根据环境修改
 NUM_AGENT = env.num_agents
@@ -48,24 +41,8 @@ agent_name_list = env.follower_agents.keys()
 
 scenario = "my_env"
 current_path = os.path.dirname(os.path.realpath(__file__))
-agent_path = current_path + "/models/" + scenario + '/'
+agent_path = current_path + "/leader_models/" + scenario + '/'
 timestamp = time.strftime("%Y%m%d%H%M%S")
-
-# 1.1 get obs_dim
-#这里只拿follower的observation_dim
-obs_dim = []
-for agent_id,agent_obs in multi_obs.items():
-    if agent_id != "leader_agent":
-        obs_dim.append(agent_obs.shape[0])
-state_dim = sum(obs_dim)
-
-# 1.2 get action_dim 
-action_dim = []
-for agent_name in agent_name_list:
-    if agent_name != "leader_agent":
-    # action_dim.append(env.action_space(agent_name).sample().shape[0])
-        action_dim.append(2)
-    
 
 agents = [ ]
 for agent in env.follower_agents.values():
@@ -77,8 +54,6 @@ for episode_i in range(NUM_EPISODE):
     print("episode",episode_i,"========================")
 
     multi_obs, infos = env.reset()
-
-    follower_episode_reward = 0
     leader_episode_reward = 0
     multi_done = {agent_name: False for agent_name in agent_name_list}
 
@@ -93,24 +68,18 @@ for episode_i in range(NUM_EPISODE):
 
         multi_actions = {}#follower_action
         for agent_id, agent in env.follower_agents.items():
-            single_obs = multi_obs[agent_id]
-            single_action = agent.get_action(single_obs)
-            multi_actions[agent_id] = single_action
-
-        # 2.2 excute action 
+            multi_actions[agent_id]=[0,0]
+            
         multi_next_obs, multi_rewards, multi_done, infos = env.step(leader_action=leader_action, follower_actions=multi_actions)
-        follower_state = multi_obs_to_state(multi_obs)#把观测量转化为全局观测量,follower不包含对leader位置的观测
-        follower_next_state = multi_obs_to_state(multi_next_obs)
 
         if step_i >= NUM_STEP -1:
-            # multi_done = {agnet_name :True for agnet_name in agent_name_list}
+        # multi_done = {agnet_name :True for agnet_name in agent_name_list}
             env.leader_agent.done = True
             multi_done["leader_agent"] = env.leader_agent.done
             for agent_id, agent in env.follower_agents.items():
                 agent.done = True
                 multi_done[agent_id] = agent.done
-            
-        # 2.3 story memory 每个agent有自己的buffer
+        
         env.leader_agent.replay_buffer.add_memo(multi_obs["leader_agent"],
                                                 multi_next_obs["leader_agent"],
                                                 np.array(multi_obs["leader_agent"]),
@@ -119,35 +88,6 @@ for episode_i in range(NUM_EPISODE):
                                                 multi_rewards["leader_agent"],
                                                 multi_done["leader_agent"]
                                                 )
-        # print("add_leader_memo")
-        for agent_id, agent in env.follower_agents.items():
-            single_obs = multi_obs[agent_id]
-            single_next_obs = multi_next_obs[agent_id]
-            single_action = multi_actions[agent_id]
-            single_reward = multi_rewards[agent_id]
-            single_done = multi_done[agent_id]
-            # print(agent_id, single_obs)
-            agent.replay_buffer.add_memo(single_obs, 
-                                        single_next_obs,
-                                        follower_state, 
-                                        follower_next_state, 
-                                        single_action, 
-                                        single_reward, 
-                                        single_done)
-
-
-
-        # 2.4  update brain every fixed steps
-        multi_batch_obses = []
-        multi_batch_next_obs = []
-        multi_batch_states= []
-        multi_batch_next_states= []
-        multi_batch_actions = []
-        multi_batch_next_actions = []
-        multi_batch_online_actions = []
-        multi_batch_rewards = []
-        multi_batch_dones = []
-
         # 2.4.1 sample a batch of memories
         current_memo_size = min(MEMORY_SIZE, total_step+1)
         if current_memo_size < BATCH_SIZE:
@@ -171,43 +111,6 @@ for episode_i in range(NUM_EPISODE):
 
         #actor output
         leader_single_batch_online_action = env.leader_agent.actor.forward(leader_batch_obses_tensor)
-
-        #follower agent update
-        for agend_id, agent in env.follower_agents.items():
-            batch_obses, batch_next_obses, batch_states, batch_next_state, batch_actions, batch_rewards, batch_dones  = agent.replay_buffer.sample(batch_idx)
-            # single+batch 
-            batch_obses_tensor = torch.tensor(batch_obses, dtype = torch.float).to(device)
-            batch_next_obses_tensor = torch.tensor(batch_next_obses, dtype = torch.float).to(device)
-            batch_states_tensor = torch.tensor(batch_states, dtype = torch.float).to(device)
-            batch_next_state_tensor = torch.tensor(batch_next_state, dtype = torch.float).to(device)
-            batch_actions_tensor = torch.tensor(batch_actions, dtype = torch.float).to(device)
-            batch_rewards_tensor = torch.tensor(batch_rewards, dtype = torch.float).to(device)
-            batch_dones_tensor = torch.tensor(batch_dones, dtype = torch.float).to(device)
-            # multiple+batch 
-            multi_batch_obses.append(batch_obses_tensor)
-            multi_batch_next_obs.append(batch_next_obses_tensor)
-            multi_batch_states.append(batch_states_tensor)
-            multi_batch_next_states.append(batch_next_state_tensor)
-            multi_batch_actions.append(batch_actions_tensor)
-            
-            #target actor output
-            # single_batch_next_actions = agent.target_actor.forward(multi_batch_next_obs)
-            single_batch_next_actions = agent.target_actor.forward(batch_next_obses_tensor)
-            multi_batch_next_actions.append(single_batch_next_actions)
-            # actor output
-            # single_batch_online_action = agent.actor.forward(multi_batch_obses)
-            single_batch_online_action = agent.actor.forward(batch_obses_tensor)
-            multi_batch_online_actions.append(single_batch_online_action)
-
-            multi_batch_rewards.append(batch_rewards_tensor)
-            multi_batch_dones.append(batch_dones_tensor)
-
-        # 采样的action
-        multi_batch_actions_tensor = torch.cat(multi_batch_actions, dim =1).to(device)
-        # 采样的next_action
-        multi_batch_next_actions_tensor = torch.cat(multi_batch_next_actions, dim =1).to(device)
-        # 采样的state经过网络对应的action
-        multi_batch_online_actions_tensor = torch.cat(multi_batch_online_actions, dim =1).to(device)
 
         # update critic and actor 
         if(total_step +1)% TARGET_UPDATE_INTERVAL == 0:
@@ -237,54 +140,9 @@ for episode_i in range(NUM_EPISODE):
             for target_param, param in zip(env.leader_agent.target_actor.parameters(), env.leader_agent.actor.parameters()):
                 target_param.data.copy_(env.leader_agent.tau * param.data + (1.0 - env.leader_agent.tau) * target_param.data)
 
-            for agent_i, agent in enumerate(env.follower_agents.values()):
-                # agent = agents[agent_i]
-
-                batch_obses_tensor = multi_batch_obses[agent_i]
-                batch_states_tensor = multi_batch_states[agent_i]
-                batch_next_state_tensor = multi_batch_next_states[agent_i]
-                batch_rewards_tensor = multi_batch_rewards[agent_i]
-                batch_dones_tensor = multi_batch_dones[agent_i]
-                batch_actions_tensor = multi_batch_actions[agent_i]
-
-                # target critic
-                critic_target_q = agent.target_critic.forward(batch_next_state_tensor,
-                                                              multi_batch_actions_tensor.detach())
-                y = (batch_rewards_tensor + (1 - batch_dones_tensor) * agent.gamma * critic_target_q).flatten()
-
-                critic_q = agent.critic.forward(batch_states_tensor, multi_batch_actions_tensor.detach()).flatten()
-
-                # critic update
-                critic_loss = nn.MSELoss()(y, critic_q)
-                agent.critic.optimizer.zero_grad()
-                critic_loss.backward()
-                # critic_loss.backward(retain_graph=True) 
-                agent.critic.optimizer.step()
-
-                # actor update 
-                actor_loss = agent.critic.forward(batch_states_tensor,
-                                                  multi_batch_online_actions_tensor.detach()).flatten()
-                actor_loss = -torch.mean(actor_loss)
-                agent.actor.optimizer.zero_grad()
-                actor_loss.backward()
-                agent.actor.optimizer.step()
-
-                # update target critic 
-                for target_param, param in zip(agent.target_critic.parameters(), agent.critic.parameters()):
-                    target_param.data.copy_(agent.tau * param.data + (1.0 - agent.tau) * target_param.data)
-                # update target actor 
-                for target_param, param in zip(agent.target_actor.parameters(), agent.actor.parameters()):
-                    target_param.data.copy_(agent.tau * param.data + (1.0 - agent.tau) * target_param.data)
-                
-
         multi_obs = multi_next_obs
         leader_reward = multi_rewards["leader_agent"]
-        for agent_id, agent in env.follower_agents.items():
-            if agend_id != "leader_agent":
-                follower_episode_reward += multi_rewards[agent_id]
-        # follower_episode_reward += sum(single_reward for single_reward in multi_rewards.values())
-        # print(f"episode_reward:{episode_reward}")
-        
+    
     # 3. render the env
     if (episode_i + 1) % 100 == 0:
         env = CustomEnv()
@@ -304,23 +162,19 @@ for episode_i in range(NUM_EPISODE):
 
                 multi_actions = {}#follower_action
                 for agent_id, agent in env.follower_agents.items():
-                    single_obs = multi_obs[agent_id]
-                    single_action = agent.get_action(single_obs)
-                    multi_actions[agent_id] = single_action
+                    
+                    multi_actions[agent_id] = [0,0]
                 # multi_next_obs, multi_rewards, multi_done, infos = env.step(multi_actions)
                 multi_next_obs, multi_rewards, multi_done, infos = env.step(leader_action=leader_action, follower_actions=multi_actions)
                     
                 multi_obs = multi_next_obs
             env.render_close()
-
+    
     # 4. save the agents' models
     if episode_i == 0:
-        highest_reward = follower_episode_reward
+        
         leader_highest_reward = leader_episode_reward
-    if follower_episode_reward > highest_reward:
-        highest_reward = follower_episode_reward
-        print(f"Highest follower reward updated at episode{episode_i}: {round(highest_reward / 512 ,2)}")
-    
+   
     if leader_episode_reward > leader_highest_reward:
         leader_highest_reward = leader_episode_reward
         print(f"Highest leader reward updated at episode{episode_i}: {round(leader_highest_reward / 512 ,2)}")
@@ -331,11 +185,6 @@ for episode_i in range(NUM_EPISODE):
             os.makedirs(agent_path)
         torch.save(env.leader_agent.actor.state_dict(), f"{agent_path}" + f"leader_agent_actor_{scenario}.pth")
 
-        for agent_i,agent in enumerate(env.follower_agents.values()):
-            # if not flag:
-            #     os.makedirs(agent_path)
-            # torch.save(agent.actor.state_dict(), f"{agent_path}" + f"agent_{agent_i}_actor_{scenario}_{timestamp}.pth")
-            torch.save(agent.actor.state_dict(), f"{agent_path}" + f"agent_{agent_i}_actor_{scenario}.pth")
-
+       
 
 
